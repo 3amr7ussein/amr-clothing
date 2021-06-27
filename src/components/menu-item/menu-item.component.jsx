5cfc7d570251f83f5e1e93155b26ad2380d8e2d3
Amr Hussein <pro3amr@gmail.com>
2021-06-28 01:41:31 +0200

    update menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
+const MenuItem = ({title , imageUrl , size ,history,match , linkUrl}) =>(
 
-export const MenuItem = ({title , imageUrl , size}) =>(
-
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
 
         <div className = "background-image" 
             style = {{
@@ -16,4 +16,6 @@ export const MenuItem = ({title , imageUrl , size}) =>(
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
-) 
+);  
+
+export default withRouter(MenuItem);
